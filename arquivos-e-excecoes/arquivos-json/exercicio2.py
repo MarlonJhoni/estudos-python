3795e34011e3400cded6f0c6be1151d0c9cacfa1
Marlon Jhoni Lima Antonelo <marlon.jhoni@hotmail.com>
2024-04-18 20:58:25 -0300

    manipulando json

--- arquivos-e-excecoes/arquivos-json/exercicio2.py
+++ arquivos-e-excecoes/arquivos-json/exercicio2.py
@@ -2,23 +2,24 @@ import json
 
 # Criar
 def escrever_json(dados, file_name):
-    with open(file_name + '.json', 'w') as arquivo:
+    with open(file_name, 'w') as arquivo:
         json.dump(dados, arquivo, ensure_ascii=False)
         arquivo.close()
 
 dados = "Ola Mundo"
-file_name = "arquivos-json/ex2-arquvio-dados"
+file_name = "arquivos-e-excecoes/arquivos-json/ex2-arquvio-dados.json"
 
-# escrever_json(dados,file_name)
+escrever_json(dados,file_name)
 
 # Ler
 def ler_json(file_name):
     data = {}
     try:
-        with open(file_name + '.json', 'r') as arquivo:
+        with open(file_name, 'r') as arquivo:
             data = json.load(arquivo)
-            return data
+            print(data)
     except FileNotFoundError:
-        
+            print('Arquivo não encontrado')
 
 ler_json(file_name)
+
